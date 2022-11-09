c7dbd0f0b3a4550ea039e766139915eb63133e20
Zoran Latković <105056049+Concept42@users.noreply.github.com>
2022-11-09 09:39:13 +0100

    cors updated

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const { Server } = require("socket.io");
 
 const io = new Server(server, {
   cors: {
-    origin: "chrome-extension://ophmdkgfcjapomjdpfobjfbihojchbko",
+    origin: "https://chat-app-front-swart.vercel.app",
     credentials: true,
   },
 });
